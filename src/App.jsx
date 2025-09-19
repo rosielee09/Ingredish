5a25c769d39f201bd2c15c91dd448f1caa8485fe
patcha-d3 <psricome@my.bcit.ca>
2025-09-19 12:04:56 -0700

    added h1

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,36 @@ import { IngredientInput } from "./components/IngredientInput";
 import { RecipeList } from "./components/RecipeList";
 
 function App() {
-	const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-	const handleIngredientsChange = (newIngredients) => {
-		setIngredients(newIngredients);
-	};
+  const handleIngredientsChange = (newIngredients) => {
+    setIngredients(newIngredients);
+  };
 
-	return (
-		<Router>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<IngredientInput
-							onIngredientsChange={
-								handleIngredientsChange
-							}
-							onSearchRecipes={() =>
-								console.log(
-									"Search recipes"
-								)
-							}
-						/>
-					}
-				/>
-				<Route
-					path='/recipes'
-					element={
-						<RecipeList
-							ingredients={
-								ingredients
-							}
-						/>
-					}
-				/>
-			</Routes>
-		</Router>
-	);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <h1 style={{ color: "red", position: "relative", zIndex: 1000 }}>
+            IngreDish
+          </h1>
+          <Route
+            path="/"
+            element={
+              <IngredientInput
+                onIngredientsChange={handleIngredientsChange}
+                onSearchRecipes={() => console.log("Search recipes")}
+              />
+            }
+          />
+          <Route
+            path="/recipes"
+            element={<RecipeList ingredients={ingredients} />}
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
