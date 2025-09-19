6688a1d3543fdc066aad0b14e33129c9f10fde4b
HyunJunggggg <hyunjung9219@gmail.com>
2025-09-19 13:09:47 -0700

    add recipecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { IngredientInput } from "./components/IngredientInput";
 import Navigation from "./components/Navigation";
+import RecipeCard from "./components/RecipeCard";
 
 function App() {
 	const [ingredients, setIngredients] = useState([]);
@@ -13,6 +14,14 @@ function App() {
 		console.log("Search recipes called with:", ingredients);
 	};
 
+	//for testing purposes
+	const sampleRecipe = {
+		title: "Juicy Burger",
+		description: "A delicious homemade burger with fresh ingredients.",
+		url: "https://example.com/burger-recipe",
+		image: "/burger.jpg"
+	  };
+	
 	return (
 		<div>
 			<Navigation />
@@ -26,6 +35,9 @@ function App() {
 			<p style={{ textAlign: "center" }}>
 				Ingredients entered: {ingredients.join(", ")}
 			</p>
+			<div className="recipe-list">
+      		  <RecipeCard recipe={sampleRecipe} />
+      		</div>
 		</div>
 	);
 }
