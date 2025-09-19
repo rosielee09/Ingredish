0abba9568c7537d8b5f269dd156a6f9d02489dfe
MaiCrespo <mcrespo1@my.bcit.ca>
2025-09-19 13:04:17 -0700

    fixed navigation css and added the app name in the navigation jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,14 +3,17 @@ import "./Navigation.css";
 export default function Navigation() {
   return (
     <nav className="nav">
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Saved</a>
-        </li>
-      </ul>
+      <div className="nav-container">
+        <div className="brand">Ingredish</div>
+        <ul className="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Saved</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
