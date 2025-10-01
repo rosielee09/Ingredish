8b0349b050dcde8b5c80594d48fbbabf9c3f62e7
patcha-d3 <psricome@my.bcit.ca>
2025-10-01 10:56:52 -0700

    nth

---
+++ .history/src/saved_20251001105450.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/Navigation.jsx";
+import "./index.css";
+import RecipeCard from "./components/RecipeCard";
+
+export default function Saved() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
