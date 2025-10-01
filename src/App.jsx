a3e8461f39089c5de77374e4a18ebb9be59244e6
Rosie Lee <blee351@my.bcit.ca>
2025-10-01 10:48:58 -0700

    style the ingredient input field

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,63 @@ import { IngredientInput } from "./components/IngredientInput";
 import { searchRecipes } from "./lib/recipe-generator";
 import Navigation from "./components/Navigation";
 import RecipeCard from "./components/RecipeCard";
+import "./app.css";
 
 function App() {
-  const [ingredients, setIngredients] = useState([]);
-  const [recipes, setRecipes] = useState([]);
+	const [ingredients, setIngredients] = useState([]);
+	const [recipes, setRecipes] = useState([]);
 
-  const handleIngredientsChange = (newIngredients) => {
-    setIngredients(newIngredients);
-  };
+	const handleIngredientsChange = (newIngredients) => {
+		setIngredients(newIngredients);
+	};
 
-  const handleSearchRecipes = async () => {
-    console.log("Search recipes called with:", ingredients);
-    const results = await searchRecipes(ingredients);
-    setRecipes(results);
-  };
+	const handleSearchRecipes = async () => {
+		console.log("Search recipes called with:", ingredients);
+		const results = await searchRecipes(ingredients);
+		setRecipes(results);
+	};
 
-  return (
-    <div>
-      <Navigation />
-      <h1 style={{ textAlign: "center" }}>Hello Ingredish!</h1>
-      <IngredientInput
-        onIngredientsChange={handleIngredientsChange}
-        onSearchRecipes={handleSearchRecipes}
-      />
+	return (
+		<div>
+			<Navigation />
+			<h1 style={{ textAlign: "center" }}>
+				Hello Ingredish!
+			</h1>
+			<IngredientInput
+				onIngredientsChange={handleIngredientsChange}
+				onSearchRecipes={handleSearchRecipes}
+			/>
 
-	 {/* For test purpose  */}
-      <div style={{ textAlign: "center", marginTop: "10px" }}>
-        <button onClick={handleSearchRecipes}>Find Recipes</button>
-      </div>
+			{/* For test purpose  */}
+			<div style={{ textAlign: "center", marginTop: "10px" }}>
+				<button onClick={handleSearchRecipes}>
+					Find Recipes
+				</button>
+			</div>
 
+			<p style={{ textAlign: "center" }}>
+				Ingredients entered: {ingredients.join(", ")}
+			</p>
 
-      <p style={{ textAlign: "center" }}>
-        Ingredients entered: {ingredients.join(", ")}
-      </p>
-	  
-      <div style={{ marginTop: "20px" }}>
-        {recipes.length > 0 ? (
-          <div className="recipe-list">
-            {recipes.map((recipe) => (
-              <RecipeCard key={recipe.id} recipe={recipe} />
-            ))}
-          </div>
-        ) : (
-          <p style={{ textAlign: "center" }}>
-            No recipes found yet. Try entering some ingredients!
-          </p>
-        )}
-      </div>
-    </div>
-  );
+			<div style={{ marginTop: "20px" }}>
+				{recipes.length > 0 ? (
+					<div className='recipe-list'>
+						{recipes.map((recipe) => (
+							<RecipeCard
+								key={recipe.id}
+								recipe={recipe}
+							/>
+						))}
+					</div>
+				) : (
+					<p style={{ textAlign: "center" }}>
+						No recipes found yet. Try
+						entering some ingredients!
+					</p>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
